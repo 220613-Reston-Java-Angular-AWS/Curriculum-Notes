cd2d329c3b98c37e631563c8181f278a52d4ea45
Tiffany Obi <tiffanyobi123@gmail.com>
2022-06-19 23:24:37 -0400

    finished demo

--- Demos/Control_Flow/src/main/java/IfElse.java
+++ Demos/Control_Flow/src/main/java/IfElse.java
@@ -1,14 +1,50 @@
 public class IfElse {
 
-    public boolean useIfElseCondition(String input){
+    public boolean checkStringLength(String input){
 
+        // the condition here says that if the length of the
+        // incoming String 'input' is greater than 7
+        // meaning there are more than 7 character in the string
+        //it will return the boolean value of true
         if(input.length() > 7){
+            return true;
 
+            //else - meaning 'otherwise'
+            //return the boolean value of false
+        } else {
+           return false;
         }
 
-
-
-
-        return false;
+        // note that we do not need a return statement at the
+        // of this method . this is because all possible conditions are
+        // already counted for
     }
+
+
+    public String printTStringsOnly(String input){
+
+        //here we made a variable equal to the
+        //first character of any String thats is put into the method
+        char firstLetter = input.charAt(0);
+        boolean check;
+        // the || operator  is how we say "OR"
+        // we are saying if the word starts with a capital or lowercase 't'
+        // we want to print this statement
+
+        if(String.valueOf(firstLetter).equals("T") || String.valueOf(firstLetter).equals("t") ){
+
+            return input + " begins with the letter 't' !" ;
+        } else {
+
+            check = false;
+        }
+        return "Did this word begin with the letter t ? : " + check ;
+    }
+
+
+
+
+
+
+
 }
---
+++ Demos/Control_Flow/src/main/java/SwitchStatements.java
@@ -0,0 +1,28 @@
+public class SwitchStatements {
+
+    public void switchStatement(int gradeCalculator){
+
+        switch (gradeCalculator/10){
+            case 10:
+                System.out.println("A+++");
+                break;
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            case 5:
+                System.out.println("F");
+                break;
+            default:
+                System.out.println("This grade is too low");
+        }
+    }
+}
